bdb8a4c48aef016c2a236a979c971d09b0923f03
unknown <stepanov.arkadyushka@inbox.ru>
2021-07-20 00:16:52 +0400

    Some fix for pipelining file and start file too

--- helpers/main.js
+++ helpers/main.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 const cheerio = require("cheerio");
 const TelegramBot = require("node-telegram-bot-api");
 const { api } = require("./Helper");
@@ -22,42 +22,28 @@ async function start() {
     },
   ]);
 
-  bot.on("/start", (msg) => {
+  bot.on("message", (msg) => {
     const id = msg.chat.id;
-    
-    bot.sendSticker(id, "https://cdn.tlgrm.ru/stickers/470/ada/470ada64-6288-316d-95b6-c84695ff0f18/192/7.webp");
 
-    bot.sendMessage(
-    id,
-    "Hello). Here is some commands",
-    {
-      reply_markup: {
-        inline_keyboard: [
-        // [{text: "Все аниме", callback_data="1"}],
-        // [{text: "Новинки", callback_data="2"}],
-        // [{text: "Фильмы", callback_data="3"}],
-        // [{text: "Новинки", callback_data="4"}],
-        // [{text: "Сериалы", callback_data="5"}],
-        // [{text: "Онгоинги", callback_data="6"}],
-        // [{text: "ТВ", callback_data="7"}],
-        // [{text: "OVA", callback_data="8"}],
-        // [{text: "ONA", callback_data="9"}],
-        // [{text: "ТВ-спешл", callback_data="10"}],
-        // [{text: "Анонсы", callback_data="11"}],
-        // [{text: "Топ аниме", callback_data="12"}],
-        // [{text: "По годам", callback_data="13"}],
-        // [{text: "Новинки", callback_data="14"}],
-        // [{text: "Все аниме", callback_data="15"}],
-        // [{text: "Новинки", callback_data="16"}]
-        [{text: "Категории", callback_data="category"}],
-        [{text: "Жанры", callback_data="types"}],
-        [{text: "Случайное аниме", callback_data="random_anime"}],
-        [{text: "Поиск по годам", callback_data="year_search"}],
-        [{text: "Поиск по названию", callback_data="name_search"}]
-        ]
-      }
-    }
-    )
+    bot.sendSticker(
+      id,
+      "https://cdn.tlgrm.ru/stickers/470/ada/470ada64-6288-316d-95b6-c84695ff0f18/192/7.webp"
+    );
+
+    // bot.sendMessage(
+    // id,
+    // "Hello). Here is some commands",
+    // {
+    //   reply_markup: {
+    //     inline_keyboard: [
+    //     [{text: "Категории", callback_data="1"}],
+    //     [{text: "Жанры", callback_data="2"}],
+    //     [{text: "Случайное аниме", callback_data="3"}],
+    //     [{text: "Поиск по годам", callback_data="4"}],
+    //     [{text: "Поиск по названию", callback_data="5"}]
+    //     ]
+    //   }
+    // })
   });
 }
 
